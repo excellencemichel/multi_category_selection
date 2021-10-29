ba5007c3f808be81673d8c8e63a2577d9fadd653
excellencemichel <bnvnmmnl@gmail.com>
2021-10-29 13:12:30 +0100

    Premier commit après l'ajout de selection de plusieur catégories

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const GetMaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,105 +16,127 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Multi Cat Select"),
+          ),
+          body: Column(
+            children: [
+              const SizedBox(height: 50),
+              CategoryFilter(),
+              const Divider(
+                height: 12,
+                color: Colors.redAccent,
+              ),
+              SelectedCategories(),
+            ],
+          ),
+        ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class SelectedCategories extends StatelessWidget {
+  final Conttoller controller = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Flexible(
+      child: Obx(
+        () => ListView.builder(
+          itemCount: controller.selecteCategories.length,
+          itemBuilder: (_, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: CategoryWidget(
+                category: controller.selecteCategories[index],
+              ),
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class CategoryFilter extends StatelessWidget {
+  final controller = Get.put(Conttoller());
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Obx(
+        () => ListView.builder(
+          itemCount: controller.categories.length,
+          itemBuilder: (_, index) {
+            return CheckboxListTile(
+              value: controller.selecteCategories
+                  .contains(controller.categories[index]),
+              onChanged: (bool? selected) {
+                controller.toggle(controller.categories[index]);
+              },
+              title: CategoryWidget(
+                category: controller.categories[index],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryWidget extends StatelessWidget {
+  final Category category;
+  const CategoryWidget({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 70,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: category.color,
+      ),
+      child: Center(
+        child: Text(
+          category.name,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+class Conttoller extends GetxController {
+  var _categories = {
+    Category(name: "Item 1", color: const Color(0xFFfe7743)): false,
+    Category(name: "Item 2", color: const Color(0xFF21d0d0)): false,
+    Category(name: "Item 5", color: const Color(0xFF895ed1)): false,
+  }.obs;
+  get categories => _categories.entries.map((e) => e.key).toList();
+  get selecteCategories => _categories.entries
+      .where(
+        (element) => element.value,
+      )
+      .map((e) => e.key)
+      .toList();
+  void toggle(Category item) {
+    _categories[item] = !(_categories[item] ?? true);
+  }
+}
+
+class Category {
+  final String name;
+  final Color color;
+  Category({
+    required this.name,
+    required this.color,
+  });
 }
